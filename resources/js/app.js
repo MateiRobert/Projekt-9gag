e6841cfef7ffc61b71cb47f1150228eefde8cdf9
Robert <robert.matei@gmx.de>
2023-08-12 01:02:38 +0200

    index.blade 3/4

--- resources/js/app.js
+++ resources/js/app.js
@@ -15,3 +15,24 @@ function previewAvatar(event) {
     }
     reader.readAsDataURL(event.target.files[0]);
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    let upvotes = document.querySelectorAll("[id^='upvote-']");
+    let downvotes = document.querySelectorAll("[id^='downvote-']");
+
+    upvotes.forEach(button => {
+        button.addEventListener('click', function() {
+            let downvoteButton = document.getElementById('downvote-' + button.id.split('-')[1]);
+            button.querySelector('svg').classList.toggle('text-blue-500');
+            downvoteButton.querySelector('svg').classList.remove('text-red-500');
+        });
+    });
+
+    downvotes.forEach(button => {
+        button.addEventListener('click', function() {
+            let upvoteButton = document.getElementById('upvote-' + button.id.split('-')[1]);
+            button.querySelector('svg').classList.toggle('text-red-500');
+            upvoteButton.querySelector('svg').classList.remove('text-blue-500');
+        });
+    });
+});
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,10 +3,9 @@
 @section('content')
 <div class="container">
     @foreach($posts as $post)
-    <div> / </div>
-        <div class="flex bg-white rounded-lg p-4 mb-4 mx-auto" style="max-width: 85%; width: 45%; height: 760px;"> 
+        <div class="flex bg-white rounded-lg p-4 mb-4 mx-auto relative" style="max-width: 85%; width: 45%; height: 760px;"> 
             <div class="flex-shrink-0 mr-3">
-                <img src="{{ $post->user->avatar_path }}" alt="{{ $post->user->name }}" class="w-10 h-10 rounded-full">
+               <img src="{{ asset('storage/' . $post->user->avatar_path) }}" alt="{{ $post->user->name }}" class="w-10 h-10 rounded-full">
             </div>
             <div class="flex-grow">
                 <div class="flex items-center text-gray-600 text-sm">
@@ -31,14 +30,42 @@
                             </x-dropdown>
                         </div>
                     @endif
-
                 </div>
                 <h2 class="text-lg font-semibold">{{ $post->title }}</h2>
                 <div class="flex justify-center my-4">
                     <a href="{{ route('posts.show', $post) }}">
-                        <img src="{{ asset('storage/posts/' . $post->image_path) }}" alt="Post Image" class="max-w-full h-auto" style="height:85%; width:90%; min-width: 100px; min-height: 85%; shadow-lg rounded-lg">
+                        <!-- Setarea dimensiunilor maxime ale imaginii -->
+                        <img src="{{ asset('storage/posts/' . $post->image_path) }}" alt="Post Image" class="max-w-full max-h-[32.5rem] object-cover w-full rounded-lg shadow-lg">
                     </a>
                 </div>
+            </div>
+            <!-- Poziționarea butoanelor în partea de jos a cardului -->
+            <div class="absolute bottom-4 left-4 flex items-center space-x-4">
+                <!-- Butoane de votare și comentarii -->
+                <button class="p-2 focus:outline-none hover:bg-blue-100" id="upvote-{{ $post->id }}">
+                    <svg class="w-8 h-8 text-gray-400 fill-current" viewBox="0 0 24 24">
+                        <path d="M7 14l5-5 5 5z"></path>
+                    </svg>
+                </button>
+                <button class="p-2 focus:outline-none hover:bg-red-100" id="downvote-{{ $post->id }}">
+                    <svg class="w-8 h-8 text-gray-400 fill-current" viewBox="0 0 24 24">
+                        <path d="M7 10l5 5 5-5z"></path>
+                    </svg>
+                </button>
+                <button class="p-2 focus:outline-none hover:bg-gray-100">
+                    <svg class="w-8 h-8 text-gray-400 fill-current" viewBox="0 0 24 24">
+                        <path d="M11.99 2C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2zm1 14h-2v-2h2v2zm0-4h-2V7h2v5z"></path>
+                    </svg>
+                </button>
+            </div>
+
+            <!-- Buton de raportare în partea dreaptă jos -->
+            <div class="absolute bottom-4 right-4">
+                <button class="p-2 focus:outline-none hover:bg-gray-200 transition-colors">
+                    <svg class="w-8 h-8 text-gray-400 fill-current hover:text-red-500" viewBox="0 0 24 24">
+                        <path d="M2,21.5 L2,16 L22,16 L22,21.5 L2,21.5 Z M2,4 L2,9.5 L22,9.5 L22,4 L2,4 Z"></path>
+                    </svg>
+                </button>
             </div>
         </div>
     @endforeach
